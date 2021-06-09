2d8095878f65d1a4d269fdbb43e111ebb8e32223
Yv-Arnel Jean Bernard <48925704+ArnelXI@users.noreply.github.com>
2021-06-09 08:22:47 -0700

    new function to split and bet while playing

--- player.py
+++ player.py
@@ -6,9 +6,19 @@ class Player:
         self.lastname = lastname
         self.solde = 1000
         self.hand = []
+        self.hand_1 = []
     
     def handle_hand(self,card1,card2):
         self.hand.append(card1,card2)
 
     def bet(self,montant):
         self.solde = self.solde - montant
+
+    def split(self):
+        self.hand_1 = self.hand[0]
+    
+    def bet(self,montant):
+        self.solde = self.solde-montant
+        return montant
+    
+    
